7494a3cc6f7ba7a3664760b3fcc697f456346406
Pravin Thokal <thokalpravinkumar@gmail.com>
2023-07-10 15:50:39 +0530

    added builder design pattern

---
+++ HeroBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern
+{
+    internal class HeroBuilder : IVehicleBuilder
+    {
+        Vehicle vehicle = null;
+
+        public HeroBuilder()
+        {
+            vehicle = new Vehicle();
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return vehicle;
+        }
+
+        public void SetAccessories()
+        {
+            vehicle.Accessories = new List<string>();
+            vehicle.Accessories.Add("Seat Cover");
+            vehicle.Accessories.Add("Helmet");
+        }
+
+        public void SetModel()
+        {
+            vehicle.Model = "Hero";
+        }
+
+        public void SetEngine()
+        {
+            vehicle.Engine = "5 Stroke";
+        }
+
+        public void SetTransmission()
+        {
+            vehicle.Transmission = "120 Km/hr";
+        }
+
+        public void SetBody()
+        {
+            vehicle.Body = "Plastic";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Builder Desing Pattern");
+
+            IVehicleBuilder heroVehicleBuilder = new HeroBuilder();
+            heroVehicleBuilder.SetTransmission();
+            heroVehicleBuilder.SetEngine();
+            heroVehicleBuilder.SetAccessories();
+            heroVehicleBuilder.SetBody();
+            heroVehicleBuilder.SetModel();
+            Vehicle herovehicle = heroVehicleBuilder.GetVehicle();
+            herovehicle.ShowInfo();
+
+            IVehicleBuilder hondaVehicleBuilder = new HondaBuilder();
+            hondaVehicleBuilder.SetTransmission();
+            hondaVehicleBuilder.SetEngine();
+            hondaVehicleBuilder.SetAccessories();
+            hondaVehicleBuilder.SetBody();
+            hondaVehicleBuilder.SetModel();
+            Vehicle hondavehicle = hondaVehicleBuilder.GetVehicle();
+            hondavehicle.ShowInfo();
+
         }
     }
 }
